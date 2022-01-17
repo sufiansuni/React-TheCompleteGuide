c302d15a3c1842a36e7232a12b0179cf03c3a6b1
Sufian Suni <sufiansuni@gmail.com>
2022-01-17 12:49:37 +0800

    project03 changed goal id to string

--- Project03-Debugging/src/App.js
+++ Project03-Debugging/src/App.js
@@ -13,7 +13,7 @@ const App = () => {
 	const addGoalHandler = (enteredText) => {
 		setCourseGoals((prevGoals) => {
 			const updatedGoals = [...prevGoals];
-			updatedGoals.unshift({ text: enteredText, id: Math.random() });
+			updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
 			return updatedGoals;
 		});
 	};
